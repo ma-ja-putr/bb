03202dac1a4797488455a6e137ac1f1bc35764da
ma-ja-putr <107904984+ma-ja-putr@users.noreply.github.com>
2022-07-14 21:59:15 +0500

    Create Baloch.py

---
+++ Baloch.py
@@ -0,0 +1,25 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import Sarfraz6
+
+    
+
+elif bit == '32bit':
+
+    import Sarfraz32
